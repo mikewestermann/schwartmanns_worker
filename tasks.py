53936f3cede28832f584f9eabbf5441762badf50
mikewestermann <54954288+mikewestermann@users.noreply.github.com>
2020-03-16 19:10:47 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -16,3 +16,42 @@ def get_random_num():
         return True
     else:
         raise Exception("Error in the worker... :(")
+        
+# task 
+
+@huey.task(retries=10, retry_delay=600)
+def send_email_task(receiver_email, subject, text):
+    sender_email = os.getenv("MY_SENDER_EMAIL")  # Your website's official email address
+    api_key = os.getenv('SENDGRID_API_KEY')
+
+    if sender_email and api_key:
+        url = "https://api.sendgrid.com/v3/mail/send"
+
+        data = {"personalizations": [{
+                    "to": [{"email": receiver_email}],
+                    "subject": subject
+                }],
+
+                "from": {"email": sender_email},
+
+                "content": [{
+                    "type": "text/plain",
+                    "value": text
+                }]
+        }
+
+        headers = {
+            'authorization': "Bearer {0}".format(api_key),
+            'content-type': "application/json"
+        }
+
+        response = requests.request("POST", url=url, data=json.dumps(data), headers=headers)
+
+        print("Sent to SendGrid")
+        print(response.text)
+    else:
+        print("No env vars or no email address.")
+        print("The email was not sent.")
+        print("If it was sent, this would be the subject: {}".format(subject))
+        print("This would be the text: {}".format(text))
+        print("And this would be the receiver: {}".format(receiver_email))
